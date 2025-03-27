da4b20bfa8e00da4cc7b3ce69e931e10c09f4fbd
nigarlyva12 <nigarlyva12@gmail.com>
2025-03-27 19:40:31 +0100

    Easy quiz done

--- Main.java
+++ Main.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+        String[][] quiz ={
+            {"What is the capital of France?", "Paris"},
+            {"What is the capital of Italy?", "Rome"},
+            {"What is the capital of Spain?", "Madrid"},
+            {"What is the capital of Germany?", "Berlin"},
+            {"What is the capital of Portugal?", "Lisbon"}
+        };
+
+        int score = 0;
+
+        for(int i=0; i<quiz.length; i++){
+            String question = quiz[i][0];
+            String answer = quiz[i][1];
+
+            System.out.println(question);
+            Scanner scanner = new Scanner(System.in);
+            String userAns = scanner.nextLine();
+
+            if(userAns.equalsIgnoreCase(answer)){
+                System.out.println("Bingo! You are correct!");
+                score++;
+            }else{
+                System.out.println("Sorry, you are wrong!");
+                System.out.println("The correct answer is: " + answer);
+            }
+        
+        }}
 }
